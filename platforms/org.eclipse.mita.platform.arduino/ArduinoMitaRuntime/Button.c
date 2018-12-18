51ede955a111042b5b76f370aa990ec84a2145d2
Robin Herrmann <Robin.Herrmann@itemis.com>
2018-12-18 14:27:38 +0100

    updated Button generator

--- platforms/org.eclipse.mita.platform.arduino/ArduinoMitaRuntime/Button.c
+++ platforms/org.eclipse.mita.platform.arduino/ArduinoMitaRuntime/Button.c
@@ -15,19 +15,21 @@
 
 
 #include "avr/io.h"
-#include <avr/interrupt.h>
 
 static void (*ButtonOnePressed)(bool val);
 static void (*ButtonOneReleased)(bool val);
 static void (*ButtonTwoPressed)(bool val);
 static void (*ButtonTwoReleased)(bool val);
 
+static void Button1InterruptCallback(void);
+static void Button2InterruptCallback(void);
+
 /**
  * Connects both buttons.
  */
 Exception_T Button_Connect(void) {
-	pinMode(2, INPUT_PULLUP);
-	pinMode(3, INPUT_PULLUP);
+	pinMode(BUTTON_1, INPUT_PULLUP);
+	pinMode(BUTTON_2, INPUT_PULLUP);
 
 	return STATUS_OK;
 }
@@ -44,8 +46,7 @@ Exception_T Button_Enable(ButtonNumber_T button, void* function, bool pressed) {
 			ButtonOneReleased = function;
 		}
 		cli();
-		EICRA |= _BV(ISC00);	// Change edge of INT0 generates Interrupt
-		EIMSK |= _BV(INTF0);	// mask INT0 interrupt
+		attachInterrupt(BUTTON_1, Button1InterruptCallback, CHANGE);
 		sei();
 		return STATUS_OK;
 	}
@@ -56,8 +57,7 @@ Exception_T Button_Enable(ButtonNumber_T button, void* function, bool pressed) {
 			ButtonTwoReleased = function;
 		}
 		cli();
-		EICRA |= _BV(ISC10); 	// Change edge of INT0 generates Interrupt
-		EIMSK |= _BV(INTF1);	// mask INT0 interrupt
+		attachInterrupt(BUTTON_2, Button2InterruptCallback, CHANGE);
 		sei();
 		return STATUS_OK;
 	}
@@ -77,11 +77,11 @@ ButtonState_T Button_GetState(ButtonNumber_T button){
 /**
  * Interrupt Service Routine for button_one. Currently, there is no debounce implemeted. 
  */
-ISR(INT0_vect){
+static void Button1InterruptCallback(void){
 	volatile int oldState = 0;
 	volatile int currentState = 0;
 	oldState = digitalRead(2);
-	currentState = digitalRead(2);
+	currentState = digitalRead(BUTTON_1);
 	if (LOW == oldState && LOW == currentState) {
 		ButtonOnePressed(true);
 	} else if (HIGH == oldState && HIGH == currentState){
@@ -92,11 +92,11 @@ ISR(INT0_vect){
 /**
  * Interrupt Service Routine for button_two. Currently, there is no debounce implemeted. 
  */
-ISR(INT1_vect){
+static void Button2InterruptCallback(void){
 	volatile int oldState = 0;
 	volatile int currentState = 0;
 	oldState = digitalRead(3);
-	currentState = digitalRead(2);
+	currentState = digitalRead(BUTTON_2);
 	if (LOW == oldState && LOW == currentState) {
 		ButtonTwoPressed(true);
 	} else if (HIGH == oldState && HIGH == currentState){
